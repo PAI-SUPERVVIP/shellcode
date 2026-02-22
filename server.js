3e7d8a170bb9bb92bafea3c92154b07884a69760
Developer <dev@example.com>
2026-02-22 03:22:05 +0000

    feat: log ENV PORT

--- server.js
+++ server.js
@@ -8,6 +8,9 @@ const path = require('path');
 const app = express();
 const server = http.createServer(app);
 const io = new Server(server);
+
+console.log("ENV PORT:", process.env.PORT);
+
 const PORT = process.env.PORT || 3000;
 
 app.use(express.static(path.join(__dirname, 'public')));
